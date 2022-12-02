7a5f475e6c25a5f03cfb451edcf7886004f31270
Keara Hayes <hayeskea@msu.edu>
2022-12-02 12:32:18 -0500

    added to notebook

---
+++ bits_n_pieces.py
@@ -0,0 +1,76 @@
+""" These are all the things that appear in Project 2, but which are not  altered 
+in Project 3. They are imported in Project 3 through this script rather than
+appearing there directly for the sake of brevity."""
+
+### rk4 routine from Project 1
+def rk4(f,t,z,h,args=()):
+    """    
+    Arguments
+        f(t,z,...)
+            function that contains the RHS of the equation dz/dt = f(t,z,...)
+    
+        t (scalar)
+            current time
+            
+        z (array-like)
+            function value
+            
+        h (scalar)
+            step size
+    
+        args (tuple, optional)
+            additional arguments to pass to f
+    
+    Returns
+        znew = z(t+h)
+    """
+   
+    if not isinstance(args,tuple):
+        args = (args,)
+
+    k1 = f(t, z, *args)
+
+    k2 = f(t + h/2, z + (h/2)*k1, *args)
+
+    k3 = f(t + h/2, z + (h/2)*k2, *args)
+
+    k4 = f(t + h, z + h*k3, *args)
+    
+    return z + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
+
+def pressure(rho, mue):
+    """
+    Arguments
+        rho
+            mass density (kg/m**3)
+        mue
+            baryon/electron ratio
+    
+    Returns
+        electron degeneracy pressure (Pascal)
+    """
+
+    p = (1/5)*(3/(8*np.pi))**(2/3)*(h**2/m_e)*(rho/(mue*m_u))**(5/3)
+    return p
+
+def density(p, mue):
+    """
+    Arguments
+        p
+            electron degeneracy pressure (Pascal)
+        mue
+            baryon/electron ratio  
+        mu
+            amu
+        h
+            Planck's constant
+        me
+            electron mass
+        
+    Returns
+        mass density (kg/m**3)
+    """
+    
+    
+    rho = mue*m_u*(5*p*(m_e/h**2)*(3/(8*np.pi))**(-2/3))**(3/5)
+    return rho
